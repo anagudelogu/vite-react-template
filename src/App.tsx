7bdbef19ffcad7fd63e3c0ed6db9e8075e7ca6e8
andres <anagudelogu@gmail.com>
2023-03-08 19:36:36 -0500

    Setup prettier and code snippets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-function App() {
-  return <div>Hello World!</div>;
+import { type FC } from 'react'
+
+const App: FC = () => {
+  return <div>Hello World!</div>
 }
 
-export default App;
+export default App
